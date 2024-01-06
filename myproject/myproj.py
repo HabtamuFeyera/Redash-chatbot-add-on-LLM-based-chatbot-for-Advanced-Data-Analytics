a420b0c551b6ce88064244dd4ad36a3acf09d6b5
HabtamuFeyera <habtamufeyera95@gmail.com>
2024-01-07 01:07:15 +0300

    Add myproject/ directory

---
+++ myproject/myproj.py
@@ -0,0 +1,27 @@
+import pandas as pd
+import psycopg2
+from sqlalchemy import create_engine
+
+# Database connection parameters
+db_params = {
+    'dbname': 'myweek3',
+    'user': 'postgres',
+    'password': 'habte05',
+    'host': 'localhost',
+    'port': '5432'
+}
+
+# CSV file path
+csv_file_path = '/home/habte/Viewership by Date/Table data.csv'
+
+# Read CSV file into a pandas DataFrame
+df = pd.read_csv(csv_file_path)
+
+# Establish a connection to the PostgreSQL database
+engine = create_engine(f'postgresql://{db_params["user"]}:{db_params["password"]}@{db_params["host"]}:{db_params["port"]}/{db_params["dbname"]}')
+
+# Insert data into PostgreSQL database
+df.to_sql('Viewership by Date', engine, index=False, if_exists='replace')
+
+# Close the database connection
+engine.dispose()
